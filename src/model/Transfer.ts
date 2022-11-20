f9a649a0fa274dc221aa7c4b17984e4471a650ee
matheus <matheus.malmeida7@gmail.com>
2022-11-20 20:29:24 -0300

    chore:(tsconfig) - Adicionando models

---
+++ src/model/Transfer.ts
@@ -0,0 +1,9 @@
+import { User } from "./User";
+
+
+export class Tranfer{
+public id:number;   
+public valor: number;
+public chave: String;
+    public usuario: User;
+}
---
+++ src/model/User.ts
@@ -0,0 +1,16 @@
+export class User{
+    public id: number
+    public nome: String;
+    public cpf: String;
+    public data_nascimento: Date;
+    public email: String;
+    public senha: String;
+    public telefone: String;
+    public logradouro: String;
+    public cep: String;
+    public numero_endereco: String;
+    public complemento: String;
+    public saldo: number;
+    public agencia: String;
+    public conta: String;
+}
